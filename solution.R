34bdcf774378e2b50f7cd7b7d2b92ff5b05eabc4
abiramikathirvel27 <abiramik@rvsgroup.com>
2026-07-19 14:19:36 +0530

    Update solution.R

--- solution.R
+++ solution.R
@@ -7,13 +7,11 @@ library(ggplot2)
 
 # TODO: Create the four plots exactly as specified.
 
-# Plot 1: scatter + smooth (overall)
-
-
-# Plot 2: scatter + smooth grouped by drv
-
-
-# Plot 3: scatter colored by drv + smooth overall
-
-
-# Plot 4: scatter colored by drv + smooth overall (color only in points)
+# Plot 1: scatter + smooth (overall) 
+plot1 <- ggplot(mpg, aes(x = displ, y = hwy)) + geom_point() + geom_smooth(se = FALSE) 
+# Plot 2: scatter + smooth grouped by drv 
+plot2 <- ggplot(mpg, aes(x = displ, y = hwy)) + geom_point() + geom_smooth(aes(group = drv), se = FALSE) 
+# Plot 3: scatter colored by drv + smooth overall 
+plot3 <- ggplot(mpg, aes(x = displ, y = hwy, color = drv)) + geom_point() + geom_smooth(se = FALSE)
+# Plot 4: scatter colored by drv + smooth overall (color only in points) 
+plot4 <- ggplot(mpg, aes(x = displ, y = hwy)) + geom_point(aes(color = drv)) + geom_smooth(se = FALSE)
